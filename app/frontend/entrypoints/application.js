aa5113a38e5ba6192b596c7267e9883d4bef1de5
Thanh Long <thanhlongnosuke@gmail.com>
2025-03-17 12:25:45 +0700

    Install eslint + supporting packages; apply prettier format
    - Install eslint
    - Add eslint to default rake task
    - Add eslint job to CI workflow
    - Auto-correct any existing issues

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -4,7 +4,7 @@ import "~/stylesheets/index.css";
 //
 //    <%= vite_client_tag %>
 //    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
+console.log("Vite ⚡️ Rails");
 
 // If using a TypeScript entrypoint file:
 //     <%= vite_typescript_tag 'application' %>
@@ -12,7 +12,10 @@ console.log('Vite ⚡️ Rails')
 // If you want to use .jsx or .tsx, add the extension:
 //     <%= vite_javascript_tag 'application.jsx' %>
 
-console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
+console.log(
+  "Visit the guide for more information: ",
+  "https://vite-ruby.netlify.app/guide/rails",
+);
 
 // Example: Load Rails libraries in Vite.
 //

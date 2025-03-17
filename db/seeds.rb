de1684466f17cd7c5700b55d246df2aa639af46a
Thanh Long <thanhlongnosuke@gmail.com>
2025-03-17 20:58:13 +0700

    Populate seed for patient

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+
+50.times do
+  Patient.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 90),
+    contact_number: Faker::Number.number(digits: 10),
+    email: Faker::Internet.unique.email,
+    address: Faker::Address.full_address
+  )
+end
+
+puts "✅ 50 Patients created!"

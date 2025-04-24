21647b464673b88fdc55b8d4d5f8c04902c77393
Thanh Long <thanhlongnosuke@gmail.com>
2025-04-24 22:10:36 +0700

    Add index view list for patient

--- app/frontend/pages/patients/index.svelte
+++ app/frontend/pages/patients/index.svelte
@@ -1,45 +1,54 @@
 <script>
   import { Link } from "@inertiajs/svelte";
-  import Layout from "../../components/Layout.svelte";
-
-
   let { patients } = $props();
+
 </script>
 
-<Layout title="Patient List">
-  <table class="table">
-    <thead>
+<div class="Generalgroup">
+  <Link href={"patients/new"} class="btn">Add new patient</Link>
+</div>
+<table class="table">
+  <thead>
+  <tr>
+    <th>ID</th>
+    <th>Full Name</th>
+    <th>Contact</th>
+    <th>Email</th>
+    <th>Actions</th>
+  </tr>
+  </thead>
+  <tbody>
+  {#each patients as patient}
     <tr>
-      <th>ID</th>
-      <th>Full Name</th>
-      <th>Date of Birth</th>
-      <th>Contact</th>
-      <th>Email</th>
-      <th>Address</th>
-      <th>Actions</th>
+      <td>{patient.id}</td>
+      <td>{patient.first_name} {patient.last_name}</td>
+      <td>{patient.contact_number}</td>
+      <td>{patient.email}</td>
+      <td>
+        <Link href={`/patients/${patient.id}`} class="btn">
+          View
+        </Link>
+        <Link href={`/patients/${patient.id}/edit`} class="btn">
+          Edit
+        </Link>
+        <Link href={`/patients/${patient.id}`} method="delete" as="button" class="btn btn--alert">
+          Destroy
+        </Link>
+      </td>
     </tr>
-    </thead>
-    <tbody>
-    {#each patients as patient}
-      <tr>
-        <td>{patient.id}</td>
-        <td>{patient.first_name} {patient.last_name}</td>
-        <td>{patient.date_of_birth}</td>
-        <td>{patient.contact_number}</td>
-        <td>{patient.email}</td>
-        <td>{patient.address}</td>
-        <td>
-          <Link href={`/patients/${patient.id}`} class="btn">
-            View
-          </Link>
-        </td>
-      </tr>
-    {/each}
-    </tbody>
-  </table>
-</Layout>
+  {/each}
+  </tbody>
+</table>
 
 <style>
+  .Generalgroup {
+    margin-bottom: 20px;
+    display: flex;
+    gap: 20px;
+    justify-content: end;
+    width: 100%;
+  }
+
   .table {
     width: 100%;
     border-collapse: collapse;
@@ -52,15 +61,5 @@
     padding: 10px;
     border-bottom: 1px solid #ddd;
     text-align: left;
-  }
-  .btn {
-    background: #1e40af;
-    color: white;
-    padding: 5px 10px;
-    text-decoration: none;
-    border-radius: 5px;
-  }
-  .btn:hover {
-    background: #1e3a8a;
   }
 </style>

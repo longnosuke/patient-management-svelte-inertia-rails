6c4d2fecc9c277fa29c729cb957566aef9d89874
Thanh Long <thanhlongnosuke@gmail.com>
2025-04-24 22:06:58 +0700

    Setup Patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -15,4 +15,9 @@
 #  updated_at     :datetime         not null
 #
 class Patient < ApplicationRecord
+  validates :first_name, :last_name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :contact_number, presence: true, format: {with: /\A\d{10}\z/, message: "must be 11"}
+  validates :date_of_birth, presence: true
+  validates :address, presence: true, length: {maximum: 255}
 end

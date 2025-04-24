69fb9b5a702db299deda6cfd2776224559bbbaa6
Thanh Long <thanhlongnosuke@gmail.com>
2025-04-24 22:08:12 +0700

    Add maintenance page

---
+++ app/frontend/pages/maintenances/index.svelte
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<div class="container">
+  <h1 class="title">Launching soon!</h1>
+  <a href="/" class="btn">Get back</a>
+</div>
+
+<style>
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100%;
+  }
+
+  .title {
+    font-size: 5em;
+    margin-bottom: 15px;
+  }
+</style>
+
+
